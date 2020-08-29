1177695d56e082e4b8c7cdbed71a1b2fb66b9894
Maciek Lubocki <mail2maciek@gmail.com>
2020-08-29 16:26:44 +0200

    New column and cards functionality

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import Card from '../Card/Card.js';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
@@ -21,7 +21,7 @@ class Column extends React.Component {
   };
   
   render() {
-    const { title, icon, cards} = this.props;
+    const { title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -35,9 +35,9 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/*<div className={styles.creator}>
+        <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={addCard} />
-        </div> */}
+        </div>
       </section>
     );
   }
