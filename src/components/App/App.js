3712dd31f9398a8cc466e22da3eb416bb48b7bf4
Maciek Lubocki <mail2maciek@gmail.com>
2020-08-29 21:59:41 +0200

    search cards functionality wip

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
 import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer.js';
 
 class App extends React.Component {
   static propTypes = {
@@ -9,19 +10,13 @@ class App extends React.Component {
     subtitle: PropTypes.node,
     lists: PropTypes.array,
   }
-
-
-  static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    lists: PropTypes.array,
-  }
   render() {
-    const { title, subtitle, lists } = this.props;
+    const { title, subtitle, lists} = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search/>
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
